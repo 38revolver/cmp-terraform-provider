505e0a8511f486534ce12c0b2a426d11a55565f3
jingyu.you <jingyu.you@samsung.com>
2022-12-20 16:17:38 +0900

    feat: add examples

---
+++ examples/resources/scp_vpc/versions.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    scp = {
+      version = "1.8.5"
+      source  = "SamsungSDSCloud/samsungcloudplatform"
+    }
+  }
+  required_version = ">= 0.13"
+}
+
+provider "scp" { 
+}
